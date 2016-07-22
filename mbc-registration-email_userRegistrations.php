30b1abf820fa6156bca40af657620948d0ea9b76
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-07-21 22:41:31 -0600

    Add environment application setting support

--- mbc-registration-email_userRegistrations.php
+++ mbc-registration-email_userRegistrations.php
@@ -14,6 +14,17 @@ define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 // See consumeMwessage for further details.
 // Necessary for parallel processing when more than one consumer is running on the same queue.
 define('QOS_SIZE', 1);
+    
+// Manage enviroment setting
+if (isset($_GET['environment']) && allowedEnvironment($_GET['environment'])) {
+    define('ENVIRONMENT', $_GET['environment']);
+} elseif (isset($argv[1])&& allowedEnvironment($argv[1])) {
+    define('ENVIRONMENT', $argv[1]);
+} elseif ($env = loadConfig()) {
+    echo 'environment.php exists, ENVIRONMENT defined as: ' . ENVIRONMENT, PHP_EOL;
+} elseif (allowedEnvironment('local')) {
+    define('ENVIRONMENT', 'local');
+}
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
@@ -28,3 +39,42 @@ $mb = $mbConfig->getProperty('messageBroker');
 $mb->consumeMessage(array(new MBC_RegistrationEmail_UserRegistration_Consumer(), 'consumeUserRegistrationQueue'), QOS_SIZE);
 
 echo '-------mbc-registration-email_useerRegistration END: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
+    
+/**
+ * Test if environment setting is a supported value.
+ *
+ * @param string $setting Requested environment setting.
+ *
+ * @return boolean
+ */
+function allowedEnvironment($setting)
+{
+    
+    $allowedEnvironments = [
+        'local',
+        'dev',
+        'prod'
+    ];
+    
+    if (in_array($setting, $allowedEnvironments)) {
+        return true;
+    }
+    
+    return false;
+}
+
+/**
+ * Gather configuration settings for current application environment.
+ *
+ * @return boolean
+ */
+function loadConfig() {
+    
+    // Check that environment config file exists
+    if (!file_exists (environment.php)) {
+        return false;
+    }
+    include('./environment.php');
+    
+    return true;
+}
